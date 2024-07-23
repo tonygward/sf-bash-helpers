c5773de5c7a44453abeed05449fba10cb0d43650
Tony Ward <tonygward.salesforce@gmail.com>
2024-07-23 10:07:55 +0100

    Use full arg names

--- testclasses-to-csv.sh
+++ testclasses-to-csv.sh
@@ -1,1 +1,1 @@
-grep '@isTest' --include="*.cls" --recursive --ignore-case --files-with-matches | xargs -L 1 basename | cut -d '.' -f 1 | sed -z 's/\n/,/g'
+        grep '@isTest' --include="*.cls" --recursive --ignore-case --files-with-matches | xargs --max-lines=1 basename | cut --delimiter='.' --fields=1 | sed 's/\n/,/g' --null-data
