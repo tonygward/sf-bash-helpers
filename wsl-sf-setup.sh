fcf884023d6a5e34be48f9708b7297f5c0166a6c
Tony Ward <tonygward@googlemail.com>
2024-07-25 09:42:26 +0100

    Added yarn

--- wsl-sf-setup.sh
+++ wsl-sf-setup.sh
@@ -3,6 +3,9 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install wget ca-certificates
 
+# install yarn
+npm install --global yarn
+
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -15,16 +18,16 @@ nvm install --lts
 npm install -g @salesforce/cli
 npm update -g @salesforce/cli
 
-# Configure Chrome on Windows for Salesforce CLI
+# configure chrome on WSL
 sudo apt-get install xdg-utils -y
 
-# Install Chrome on Windows
+# install chrome
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --fix-missing ./google-chrome-stable_current_amd64.deb -y
 cd ~/
 
-# Java Jdk 21
+# java jdk 21
 sudo apt install openjdk-21-jdk -y
 # Salesforce Extension Folder: /usr/lib/jvm/java-21-openjdk-amd64/
 
